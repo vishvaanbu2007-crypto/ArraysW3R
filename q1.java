a8ca22426c2e41ca77f70de24c3b6fe1f98b241c
Vishva Anbhazhakan <vishvaanbu2007@gmail.com>
2026-03-15 12:25:35 +0000

    Sorted arrays

---
+++ q1.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class q1{
+    public static void main(String[] args){
+        int[] arr1={1,3,24,5,65,67};
+        Arrays.sort(arr1);
+        System.out.println("sorted numeric array"+ Arrays.toString(arr1));
+
+        String[] arr2 = {"Sita","Rama","Vibhishana","Vishva"};
+        Arrays.sort(arr2);
+        System.out.println("Sorted string array"+ Arrays.toString(arr2));
+
+        
+    }
+}

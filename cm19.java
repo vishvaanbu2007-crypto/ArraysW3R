83657cd82a6732c7e56e9e7c3d57155984459082
vishvaanbu2007-crypto <vishvaanbu2007@gmail.com>
2026-03-19 18:11:41 +0530

    cm19

--- cm19.java
+++ cm19.java
@@ -2,9 +2,7 @@ import java.util.*;
 public class cm19{
     public static void main(String[] args){
         int r,c,i,j;
-        int[][] mat1=new int[r][c];
-        int[][] mat2=new int[r][c];
-        int[][] mat3=new int[r][c];
+        
         Scanner s=new Scanner(System.in);
         
         System.out.println("Enter number of rows: ");
@@ -12,6 +10,9 @@ public class cm19{
         System.out.println("Enter number of columns: ");
         c=s.nextInt();
         System.out.println("Enter elements of matrix 1: ");
+        int[][] mat1=new int[r][c];
+        int[][] mat2=new int[r][c];
+        int[][] mat3=new int[r][c];
         for(i=0;i<r;i++){
             for(j=0;j<c;j++){
                 mat1[i][j]=s.nextInt();
@@ -23,10 +24,15 @@ public class cm19{
                 mat2[i][j]=s.nextInt();
             }
         }
-        mat3=mat1+mat2;
+        
+        for(i=0;i<r;i++){
+            for(j=0;j<c;j++){
+                mat3[i][j]=mat1[i][j]+mat2[i][j];
+            }
+        }
         for(int[] row:mat3){
-            for(int element:row){
-                    System.out.print(element+" ");
+            for(int element : row){
+                System.out.print(element+" ");
             }
             System.out.println();
         }
